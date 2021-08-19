d2c7fbcdb3468ae9d7a54c2dd94e7fc09151591f
leejy0978 <leejy0978@naver.com>
2021-08-19 10:42:04 +0900

    Load 21.08.18

--- src/main/java/org/example/project/service/posts/PostsService.java
+++ src/main/java/org/example/project/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package org.example.project.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.example.project.domain.posts.Posts;
 import org.example.project.domain.posts.PostsRepository;
+import org.example.project.web.dto.PostsListResponseDto;
 import org.example.project.web.dto.PostsResponseDto;
 import org.example.project.web.dto.PostsSaveRequestDto;
 import org.example.project.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
